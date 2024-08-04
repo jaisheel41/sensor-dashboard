7b2a069c7b65bdf88594510b4e10d4cc2e8cdaab
jaisheel41 <jaisheel42@gmail.com>
2024-08-04 16:24:51 +0100

    Add config-overrides.js file

---
+++ config-overrides.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = function override(config) {
+  config.resolve.fallback = {
+    ...config.resolve.fallback,
+    path: require.resolve('path-browserify'),
+  };
+  return config;
+};
