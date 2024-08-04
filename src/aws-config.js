ddfa4d0bf5f60528e49e4a3210b01bd7f6f18150
jaisheel41 <jaisheel42@gmail.com>
2024-08-04 17:09:22 +0100

    Ensure AWS SDK configuration for DynamoDB

--- src/aws-config.js
+++ src/aws-config.js
@@ -1,15 +1,12 @@
 // src/aws-config.js
-import AWS from 'aws-sdk';
-import dotenv from 'dotenv';
+import Amplify from 'aws-amplify';
 
-dotenv.config();
-
-AWS.config.update({
-  region: process.env.REACT_APP_AWS_REGION,
-  accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
+Amplify.configure({
+  Auth: {
+    region: process.env.REACT_APP_AWS_REGION,
+    accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
+  },
 });
 
-const dynamodb = new AWS.DynamoDB.DocumentClient();
-
-export default dynamodb;
+export default Amplify;
--- src/fetchData.js
+++ src/fetchData.js
@@ -1,5 +1,13 @@
 // src/fetchData.js
-import dynamodb from './aws-config';
+import AWS from 'aws-sdk';
+
+AWS.config.update({
+  region: process.env.REACT_APP_AWS_REGION,
+  accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
+});
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 const fetchData = async () => {
   const params = {
