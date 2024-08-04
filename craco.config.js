feb2616e34522964f810fe53800d64d46f029673
jaisheel41 <jaisheel42@gmail.com>
2024-08-04 16:41:46 +0100

    Add process polyfill and update craco config

--- craco.config.js
+++ craco.config.js
@@ -9,7 +9,13 @@ module.exports = {
         os: require.resolve('os-browserify/browser'),
         crypto: require.resolve('crypto-browserify'),
         stream: require.resolve('stream-browserify'),
+        process: require.resolve('process/browser'), // Add this line
       };
+      webpackConfig.plugins = (webpackConfig.plugins || []).concat([
+        new webpack.ProvidePlugin({
+          process: 'process/browser',
+        }),
+      ]);
       return webpackConfig;
     },
   },
